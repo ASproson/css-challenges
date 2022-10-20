8d325349cbe8d3deb11504a066277df0c38ec07b
atlas <a.sproson@outlook.com>
2022-10-20 19:48:35 +0100

    Initial design layout

--- src/components/AudioEqualizer.jsx
+++ src/components/AudioEqualizer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const AudioEqualizer = () => {
-  return <div>Hello world</div>;
+  return (
+    <div className="p-10 flex justify-center">
+      {/* Canvas */}
+      <div className="border-2 border-black rounded-md w-[800px] h-[500px]">
+        {/* Inner Canvas */}
+        <div className="bg-black h-full w-full"></div>
+      </div>
+    </div>
+  );
 };
 
 export default AudioEqualizer;
